8e9e834e54c31f57f9a4b7a019d06e6ac2013bca
MahmudovMuhammadrizo24 <mahmudovmuhammadrizo877@gmail.com>
2023-04-08 09:51:40 +0500

    add js and css code

--- main.js
+++ main.js
@@ -14,19 +14,11 @@
 */
 
 
+/*
 const input = document.querySelector('.input');
 const addIBtn = document.querySelector('.add_item_btn');
 const container = document.querySelector(".container");
-addIBtn.addEventListener("dblclick", function () {
 
-    const liner = createIcon("fa-check-circle", "#80cfa9", () => {
-        itemName.style.textDecoration = "line-through";
-        newItem.style.background = "white";
-        item.style.pointerEvents = "all";
-    });
-
-
-})
 addIBtn.addEventListener("click", function () {
     if (input.value.trim() != "") {
         const newItem = document.createElement("div");
@@ -47,7 +39,6 @@ addIBtn.addEventListener("click", function () {
 
         const edit = createIcon("fa-edit", "#007bff", () => {
             input.value = itemName.textContent;
-
             container.removeChild(newItem);
 
         });
@@ -80,6 +71,179 @@ function clearItems() {
 
 document.querySelector('.clear-item').addEventListener('click', clearItems);
 input.value = '';
+*/
+/*
+const input = document.querySelector('.input');
+const addBtn = document.querySelector('.add_item_btn');
+const container = document.querySelector('.container');
 
+addBtn.addEventListener('click', addItem);
+
+function addItem() {
+    if (input.value.trim() === '') {
+        alert('Please enter a task');
+        return;
+    }
+
+    const newItem = document.createElement('div');
+    newItem.classList.add('item');
+
+    const itemName = document.createElement('p');
+    itemName.classList.add('item-name');
+    itemName.textContent = input.value.trim();
+
+    const iconsContainer = document.createElement('div');
+    iconsContainer.classList.add('icons');
+
+    const liner = createIcon('fa-check-circle', '#80cfa9', () => {
+        itemName.style.textDecoration = 'line-through';
+        newItem.style.color = 'blue';
+        newItem.style.pointerEvents = 'none';
+        liner.addEventListener('dblclick', function () {
+            itemName.style.textDecoration = "line-through";
+        });
+    });
+
+    const edit = createIcon('fa-edit', '#007bff', () => {
+        input.value = itemName.textContent;
+        container.removeChild(newItem);
+    });
+
+    const timesCircle = createIcon('fa-times-circle', 'crimson', () => {
+        container.removeChild(newItem);
+    });
+
+    iconsContainer.append(liner, edit, timesCircle);
+    newItem.append(itemName, iconsContainer);
+    container.appendChild(newItem);
+
+    input.value = '';
+}
+
+function createIcon(iconClass, color, clickHandler) {
+    const icon = document.createElement('i');
+    icon.classList.add('far', iconClass);
+    icon.style.color = color;
+    icon.addEventListener('click', clickHandler);
+    return icon;
+}
+
+function clearItems() {
+    container.innerHTML = '';
+}
+
+const clearBtn = document.querySelector('.clear-item');
+clearBtn.addEventListener('click', clearItems);
+*/
+
+const input = document.querySelector('.input');
+const addBtn = document.querySelector('.add_item_btn');
+const container = document.querySelector('.container');
+const clearBtn = document.querySelector('.clear-item');
+
+addBtn.addEventListener('click', addItem);
+
+function addItem() {
+    if (input.value.trim() === '') {
+        alert('Please enter a task');
+        return;
+    }
+
+    const newItem = document.createElement('div');
+    newItem.classList.add('item');
+
+    const itemName = document.createElement('p');
+    itemName.classList.add('item-name');
+    itemName.textContent = input.value.trim();
+
+    const iconsContainer = document.createElement('div');
+    iconsContainer.classList.add('icons');
+
+    const liner = createIcon('fa-check-circle', '#80cfa9', () => {
+        itemName.style.textDecoration = 'line-through';
+        newItem.style.color = 'blue';
+        newItem.style.pointerEvents = 'none';
+    });
+
+    // Add double-click event listener to task name
+    itemName.addEventListener('dblclick', function () {
+        itemName.style.textDecoration = "line-through";
+    });
+
+    const edit = createIcon('fa-edit', '#007bff', () => {
+        input.value = itemName.textContent;
+        container.removeChild(newItem);
+    });
+
+    const timesCircle = createIcon('fa-times-circle', 'crimson', () => {
+        container.removeChild(newItem);
+    });
+
+    iconsContainer.append(liner, edit, timesCircle);
+    newItem.append(itemName, iconsContainer);
+    container.appendChild(newItem);
+
+    // Save items to local storage
+    const items = JSON.parse(localStorage.getItem('items')) || [];
+    items.push(input.value.trim());
+    localStorage.setItem('items', JSON.stringify(items));
+
+    input.value = '';
+}
+
+function createIcon(iconClass, color, clickHandler) {
+    const icon = document.createElement('i');
+    icon.classList.add('far', iconClass);
+    icon.style.color = color;
+    icon.addEventListener('click', clickHandler);
+    return icon;
+}
+
+function clearItems() {
+    container.innerHTML = '';
+    localStorage.removeItem('items');
+}
+
+// Load items from local storage on page load
+window.onload = function () {
+    const items = JSON.parse(localStorage.getItem('items')) || [];
+    for (let i = 0; i < items.length; i++) {
+        const newItem = document.createElement('div');
+        newItem.classList.add('item');
+
+        const itemName = document.createElement('p');
+        itemName.classList.add('item-name');
+        itemName.textContent = items[i];
+
+        const iconsContainer = document.createElement('div');
+        iconsContainer.classList.add('icons');
+
+        const liner = createIcon('fa-check-circle', '#80cfa9', () => {
+            itemName.style.textDecoration = 'line-through';
+            newItem.style.color = 'blue';
+            newItem.style.pointerEvents = 'none';
+        });
+
+        // Add double-click event listener to task name
+        itemName.addEventListener('dblclick', function () {
+            itemName.style.textDecoration = "line-through";
+        });
+
+        const edit = createIcon('fa-edit', '#007bff', () => {
+            input.value = itemName.textContent;
+            container.removeChild(newItem);
+        });
+
+        const timesCircle = createIcon('fa-times-circle', 'crimson', () => {
+            container.removeChild(newItem);
+        });
+
+        iconsContainer.append(liner, edit, timesCircle);
+        newItem.append(itemName, iconsContainer);
+        container.appendChild(newItem);
+    }
+}
+
+clearBtn.addEventListener('click', clearItems);
 
 
